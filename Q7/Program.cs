05c7823dc5a312d25b62321c18f13688e7d7bf97
Nikita Bochkovskiy <nikita.bochkovsky@yandex.ru>
2022-04-24 20:19:46 +0600

    Задание 7.5.3

--- Q7/Program.cs
+++ Q7/Program.cs
@@ -6,30 +6,25 @@ namespace Q7
 	{
 		static void Main(string[] args)
 		{
+			int num1 = 3;
+			int num2 = 58;
 
+			Helper.Swap(ref num1,ref num2);
+
+			Console.WriteLine(num1); //58
+			Console.WriteLine(num2); //3
 		}
 	}
-	class IndexingClass
+	class Helper
 	{
-		private int[] array;
-
-		public IndexingClass(int[] array)
-		{
-			this.array = array;
-		}
-
-        public int this[int index]
+     public static void Swap (ref int a,ref int b )
         {
-			get
-			{
-				return array[index];
-			}
-
-			set
-			{
-				array[index] = value;
-			}
-		}
+			int num3 = a;
+			a = b;
+			b = num3;
+        }
     }
+
+
 }
 
