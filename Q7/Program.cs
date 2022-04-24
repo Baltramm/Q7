e667c21c0062381dec6b02fe636ee1655defcfa6
Nikita Bochkovskiy <nikita.bochkovsky@yandex.ru>
2022-04-24 20:34:41 +0600

    Задание 7.5.9

--- Q7/Program.cs
+++ Q7/Program.cs
@@ -6,23 +6,41 @@ namespace Q7
 	{
 		static void Main(string[] args)
 		{
-			
+			int num1 = 7;
+			int num2 = -13;
+			int num3 = 0;
+
+			Console.WriteLine(num1.GetNegative()); //-7
+			Console.WriteLine(num1.GetPositive()); //7
+			Console.WriteLine(num2.GetNegative()); //-13
+			Console.WriteLine(num2.GetPositive()); //13
+			Console.WriteLine(num3.GetNegative()); //0
+			Console.WriteLine(num3.GetPositive()); //0
 		}
 	}
-	class Obj
+	static class Int 
 	{
-		public string Name;
-		public string Description;
-
-		public static string Parent;
-		public static int DaysInWeek;
-		public static int MaxValue;
-
-		static Obj()
+		public static int GetNegative(this int num)
 		{
-			Parent = "System.Object";
-			DaysInWeek = 7;
-			MaxValue = 2000;
+			if (num > 0)
+			{
+				return -num;
+			}
+			else
+			{
+				return num;
+			}
+		}
+		public static int GetPositive(this int num)
+		{
+			if (num < 0)
+			{
+				return -num;
+			}
+			else
+			{
+				return num;
+			}
 		}
 	}
 
